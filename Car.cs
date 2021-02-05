fe54d1e10017cafad3104a08245be453e54e8f79
volunteerocks <66223142+aabit@users.noreply.github.com>
2021-02-04 23:37:14 -0600

    Added basic C# classes exercise

---
+++ Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace BasicClassExercise
+{
+    public class Car
+    {
+       
+
+        public Car() // default constructor
+        {
+        }
+
+        public Car(string name, string model, int year) // custom constructor
+        {
+            Name = name;
+            Model = model;
+            Year = year;
+        }
+
+       
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BasicClassExercise
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Name = "Road Master";
+            myCar.Model = "Jeep Wrangler Unlimited Sahara";
+            myCar.Year = 2021;
+
+
+            Console.WriteLine($"My car's name is { myCar.Name } and it's a {myCar.Year} {myCar.Model}");
+
+
+        }
+
+
+    }
+}
